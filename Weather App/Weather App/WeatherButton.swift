d4d1cbb319c5da91832abcae62e2527488ef2515
Lucas Alcantara <lucasprestesmatos@hotmail.com>
2024-10-24 03:09:56 -0300

    Created basic UI

---
+++ Weather App/Weather App/WeatherButton.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherButton.swift
+//  Weather App
+//
+//  Created by Lucas  Alcantara  on 24/10/24.
+//
+import SwiftUI
+
+struct WeatherButton: View {
+    
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text("Change Day Time")
+            .frame(width: 280, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Lucas  Alcantara  on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
